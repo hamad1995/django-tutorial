cd143d99c97b2366892a0a6b008031e4185d895a
Hammad AlHammad <h.alhammad@tamkeentech.sa>
2022-04-11 12:19:19 +0300

    initial commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import include , path
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
+
+
+
+
+
+
+
+
+# from django.contrib import admin
+# from django.urls import include, path
+
+# urlpatterns = [
+#     path('polls/', include('polls.urls')),
+#     path('admin/', admin.site.urls),
+# ]
